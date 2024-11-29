2c3a29cbb01959748d61f31066cef8446e98a8cc
Kernel-Level <y866859@gmail.com>
2024-11-29 09:28:18 -0500

    Create funny.lua

---
+++ funny.lua
@@ -0,0 +1,7 @@
+
+_G.Message = Instance.new("Color3Value")
+_G.Message.Value = Color3.fromRGB(255,255,0)
+_G.Message.Name = "Hello ibrahim its me..."
+_G.Message.Parent = game.Players.LocalPlayer.Character["@Alerts"]
+
+
